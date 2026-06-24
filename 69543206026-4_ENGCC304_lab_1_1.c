f24e01cf029edc3d10dad870bce3e0b7735799f1
Pornsuwan <dxkdwngphrsuwrrn@gmail.com>
2026-06-24 16:33:34 +0700

    Lab1

---
+++ 69543206026-4_ENGCC304_lab_1_1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+char Name[20];
+scanf ("%s",Name);
+printf("Hello, %s\n",Name);
+printf("Welcome to C Programming\n");
+return 0;
+}
--- test.c
+++ test.c
@@ -2,4 +2,6 @@
 
 int main() {
     printf("Hello, World!\n");
+
+    return 0;
 }
